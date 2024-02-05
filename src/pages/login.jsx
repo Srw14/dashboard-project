c38c33f776b6a72ee9bf1ecd63c8ed924acd7220
srw14 <ggg050474@gmail.com>
2024-02-06 02:33:37 +0700

    first commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,15 +3,16 @@ import "./login.css";
 function login() {
   return (
     <>
-      <img
+      {/* <img
         style={{ width: "100%", height: "100%" }}
         src="/src/img/bg-img.jpg"
-      />
+      /> */}
       <body>
         <div class="login-from">
           <div class="header-text">Login</div>
-          <input placeholder="Your Username" type="text" />
-          <input placeholder="Your Password" type="password" />
+          <input placeholder="Your Username" type="text" className="Username" />
+          <input placeholder="Your Password" type="Password" className="a" />
+
           <button>login</button>
         </div>
       </body>
